55b40009b89dbfa8c476f7879c91259d1a3a7a5b
Gaetan <Gaetan.boismal@ensimag.imag.fr>
2013-11-03 17:08:32 +0100

    Ajout de la fenetre prinicipale et celle d'erreur

--- tracher-boik/src/com/trackerboik/bdd/Main.java
+++ tracher-boik/src/com/trackerboik/bdd/Main.java
@@ -6,18 +6,18 @@ import java.sql.SQLException;
 public class Main {
 	 
     public static void main(String[] args) {
-        Connexion connexion = new Connexion("");
-        connexion.connect();
-//        ResultSet resultSet = connexion.query("SELECT * FROM PlayerPS");
-//        try {
-//            while (resultSet.next()) {
-//                System.out.println("Player : "+resultSet.getString("PlayerID"));
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
- 
-        connexion.close();
+//        Connexion connexion = new Connexion("");
+//        connexion.connect();
+////        ResultSet resultSet = connexion.query("SELECT * FROM PlayerPS");
+////        try {
+////            while (resultSet.next()) {
+////                System.out.println("Player : "+resultSet.getString("PlayerID"));
+////            }
+////        } catch (SQLException e) {
+////            e.printStackTrace();
+////        }
+// 
+//        connexion.close();
     }
  
 }
